bfd00ef26ab06cedb497c60548c8930e5b0bc300
shenskho <amir.pampay@gmail.com>
2026-06-18 16:42:41 +0330

    nastedTernary

--- src/components/TaskCounter.jsx
+++ src/components/TaskCounter.jsx
@@ -1,32 +1,43 @@
 import { useState } from "react";
 function TaskCounter() {
   const [count, setCount] = useState(0);
-  const increaseClick = () => {
-    setCount(count + 1);
+  const handleIncreaseClick = () => {
+    setCount((prevCount) => prevCount + 1);
   };
-  const resetClick = () => {
-    setCount(0)
+  const handleResetClick = () => {
+    setCount(0);
   };
-  const decreaseClick = () =>{
-    setCount(
-      (prevCount)=> prevCount > 0 ? prevCount-1 : 0
-    )
-    
-  }
+  const handleDecreaseClick = () => {
+    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+  };
+  const counterMessageClass = count === 0 ?"counter-message counter-message--empty": count < 5 ? "counter-message counter-message--low" : "counter-message counter-message--high"
+  const counterMessage =
+    count === 0
+      ? "هیچ تسکی وجود ندارد"
+      : count < 5
+        ? "تعداد تسک ها کم است "
+        : "تسک های زیادی داری";
   return (
     <section className="task-counter">
       <h2>Task counter</h2>
       <p>current value : {count}</p>
+      <p className={counterMessageClass}>{counterMessage}</p>
       <div className="counter-actions">
-        <button type="button" onClick={increaseClick}>
-        Increase
-      </button>
-      <button type="button" onClick={resetClick}>
-        Reset
-      </button>
-      <button type="button" onClick={decreaseClick} disabled={count===0}>
-        Decrease
-      </button>
+        <button type="button" onClick={handleIncreaseClick}>
+          Increase
+        </button>
+        {count > 0 && (
+          <button type="button" onClick={handleResetClick}>
+            Reset
+          </button>
+        )}
+        <button
+          type="button"
+          onClick={handleDecreaseClick}
+          disabled={count === 0}
+        >
+          Decrease
+        </button>
       </div>
     </section>
   );
